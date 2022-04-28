96813c409d255aea8e0a8278a9dc145fdb180b3d
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-04-28 23:00:56 +0600

    firebase setup and other installations done

---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1nQJwYk2O3jmw7KVDC_-ODn04OcLXpVI",
+  authDomain: "e-warehouse-client.firebaseapp.com",
+  projectId: "e-warehouse-client",
+  storageBucket: "e-warehouse-client.appspot.com",
+  messagingSenderId: "245796065878",
+  appId: "1:245796065878:web:b190b1d2ee79ca409c21f7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
